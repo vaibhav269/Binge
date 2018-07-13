07ba0838df30859ce2699f8e180a2b9763e9b25d
Vaibhav <vaibhav.verma2697@gmail.com>
2018-07-13 23:44:29 +0530

    fixed index.html for production

--- server.js
+++ server.js
@@ -4,9 +4,9 @@ const path= require('path');
 
 app.use(express.static(__dirname + '/dist'));
 
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 5000);
 
 app.get('/*',function(req,res){
-    res.sendFile(path.join(__dirname + '/dist/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/binge-app/index.html'));
 });
 console.log("Server running");
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseCred: {
+    apiKey: "AIzaSyDLAnATlnf23w5xhAebPcFQw2POdrGK-NQ",
+    authDomain: "bingetesting.firebaseapp.com",
+    databaseURL: "https://bingetesting.firebaseio.com",
+    projectId: "bingetesting",
+    storageBucket: "bingetesting.appspot.com",
+    messagingSenderId: "254566168359"
+  }
 };
